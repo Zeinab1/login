f5467c998fb053ef9f0e9b15911f103361d2e3fc
Zeinab Gamal <zeinab.gamal512@gmail.com>
2019-11-02 21:34:38 +0200

    Updates

---
+++ precache-manifest.91384710308a35dd8dc4dd4fbf38e4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a63ef79929d145408da62b748052290",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "17e8cf549b1ee108a6b9",
+    "url": "/login/static/css/main.4cb8dd28.chunk.css"
+  },
+  {
+    "revision": "519667f9a2d64fe4163e",
+    "url": "/login/static/js/2.1ad90ff8.chunk.js"
+  },
+  {
+    "revision": "17e8cf549b1ee108a6b9",
+    "url": "/login/static/js/main.100a7dad.chunk.js"
+  },
+  {
+    "revision": "e83f0580dcc500e79955",
+    "url": "/login/static/js/runtime-main.423fae98.js"
+  },
+  {
+    "revision": "62108e0d97d82e5f48151500b07f0926",
+    "url": "/login/static/media/developer.62108e0d.png"
+  }
+]);
